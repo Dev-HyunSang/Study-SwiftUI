37acfc3fc79fd9a39c4656abacf9997b0f76de33
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-01-03 18:37:32 +0900

    [UPDATE🛠] INIT 🛠 Project !

---
+++ SwiftUI_tutorial_#2/SwiftUI_tutorial_#2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_#2
+//
+//  Created by Jasper Park on 2021/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_tutorial_#2/SwiftUI_tutorial_#2/SwiftUI_tutorial__2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorial__2App.swift
+//  SwiftUI_tutorial_#2
+//
+//  Created by Jasper Park on 2021/01/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorial__2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
